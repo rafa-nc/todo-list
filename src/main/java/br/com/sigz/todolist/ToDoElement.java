739ee5bdae974ff11784ec10053cb9f3f1d911f9
Rafael <rafael.costa04@outlook.com>
2022-02-03 20:40:54 -0300

    adicionando delete e update

--- src/main/java/br/com/sigz/todolist/ToDoElement.java
+++ src/main/java/br/com/sigz/todolist/ToDoElement.java
@@ -20,6 +20,10 @@ public class ToDoElement {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getNome() {
         return nome;
     }
--- src/main/java/br/com/sigz/todolist/ToDoListController.java
+++ src/main/java/br/com/sigz/todolist/ToDoListController.java
@@ -16,15 +16,26 @@ public class ToDoListController {
     public List<ToDoElement> getList() {
         return repository.findAll();
     }
+
     @PostMapping
     public void creat(@RequestBody ToDoElement element) {
         element.setData(LocalDateTime.now());
         repository.save(element);
+    }
 
+    @DeleteMapping(value = "{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
 
     }
 
+    @PutMapping(value = "{id}")
 
+    public void update(@PathVariable Long id, @RequestBody ToDoElement element) {
+        element.setId(id);
+        repository.save(element);
+
+    }
 
 
 }
